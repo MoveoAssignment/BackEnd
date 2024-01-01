bf555ecdb6b445b8d69d24414f597161785fcae6
YuvalOfek1 <yuvalofek97@gmail.com>
2024-01-01 16:40:14 +0200

    check

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const codeblockRoutes = require("./routes/Codeblock.routes");
 const connectDB = require("./config/mongo");
-const {Server} = require("socket.io");
+const socketIO = require("socket.io");
 const { setSocket } = require("./utils/socketService");
+const http = require("http");
 
 const app = express();
 dotenv.config();
@@ -16,21 +17,41 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/codeblocks", codeblockRoutes);
 
-const server = require("http").createServer(app);
-const io = new Server(server, {
-  cors: { origin: "*:*", methods: ["GET", "POST"] },
-});
+const server = http.createServer(app);
+const io = socketIO(server)
 
 setSocket(io);
 const { handleClient } = require("./utils/socket");
-handleClient(io);
+let count=0;
+io.on("connection", (socket) => {
+  console.log("New WebSocket Connection... " + ++count);
+
+  // Increment count and emit the updated count to all clients
+  io.emit("count", count);
+  console.log("count: ", count);
+
+  // Listen for incoming chat messages
+  socket.on("message", (message) => {
+    // Broadcast the sanitized message to all connected clients
+    io.emit("message", message);
+  });
+
+  // Listen for disconnect event
+  socket.on("disconnect", () => {
+    // Decrement count and emit the updated count to all clients
+    count--;
+    io.emit("count", count);
+    console.log("Client has closed the socket");
+    io.emit("message", "The other user has left the code block");
+  });
+});
 
 // using socket comunicatin for the chat.
-server.listen(SOCKET_PORT, () => {
-  console.log(`Socket Server is running on port ${SOCKET_PORT}`);
-  connectDB();
-});
-
-app.listen(PORT, () => {
+connectDB();
+server.listen(PORT, () => {
   console.log(`Server Running on Port: http://localhost:${PORT}`);
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Server Running on Port: http://localhost:${PORT}`);
+// });
